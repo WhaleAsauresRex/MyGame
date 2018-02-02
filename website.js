35f04d4865baba4b6d7dc3cd9cdcdc510e73e1ac
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2018-02-02 10:54:17 -1000

    Add files via upload

--- website.js
+++ website.js
@@ -1,1 +1,9 @@
 //hello
+function SetList() {
+  var UserInput= document.getElementById('UserInput').value;
+  var CreateList = document.createElement('li');
+  var UserInfo = document.createTextNode(UserInput);
+  CreateList.appendChild(UserInfo);
+  var inputIntoUnOrdered = document.getElementsByTagName('ol')[0];
+  inputIntoUnOrdered.appendChild(CreateList);
+}
